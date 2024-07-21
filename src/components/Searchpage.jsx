451f5b7c7bca6409a4a653097013a33eb9755e7d
HELEN-DEE <helendeee12@gmail.com>
2024-07-21 16:24:44 +0100

    Fixed search input positioning and added the desired font

--- src/components/Searchpage.jsx
+++ src/components/Searchpage.jsx
@@ -5,9 +5,9 @@ const Searchpage = () => {
     return (
         <section className='bg-[#dadbf1] py-24 '>
             <h1 className='lg:text-7xl text-3xl font-medium text-center pb-8'>How can we help?</h1>
-            <div className='flex justify-center'>
-            <input type="search" name="search" id="search" placeholder='Search' className='py-4 px-3 text-xl lg:w-[55%]  w-[70%] border border-black focus:border-[#4C5FD5] rounded focus:outline-none shadow-md' />
-            <span className='relative right-10 py-4'>
+            <div className='flex justify-between lg:w-[55%]  w-[70%] gap-2 mx-auto bg-white items-center border border-black focus:border-[#4C5FD5] rounded shadow-md py-4'>
+            <input type="search" name="search" id="search" placeholder='Search' className=' px-3 text-xl  focus:outline-none flex-1' />
+            <span className=''>
                 <IoIosArrowRoundForward size={35} className='hover:text-[#4C5FD5]'/>
             </span>
             </div>
